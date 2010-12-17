a3895c28e233da2a0e03c5d376987e95d66fe0ac
Fabio Erculiani <lxnay@sabayon.org>
2010-12-17 14:11:09 +0100

    [remaster/gaming] add more games, remove more crap

---
+++ scripts/remaster_gaming_inner_chroot_script_after.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# FUTURE NOTE:
+# this might get merged into remaster_generic_inner_chroot_script_after.sh
+# once the Gaming Edition will be rebased on top of SpinBase, now it's based
+# based on GNOME.
+
+# Not shipping xbmc
+rm /etc/skel/Desktop/xbmc.desktop || exit 1
+
+# Copy games icons on the desktop
+for desktop_file in $(grep -rl "Categories=Game" /usr/share/applications/*); do
+	desktop_name=$(basename "${desktop_file}")
+	cp "${desktop_file}" "/etc/skel/Desktop/${desktop_name}" || exit 1
+	chmod 755 "/etc/skel/Desktop/${desktop_name}" || exit 1
+	chown root "/etc/skel/Desktop/${desktop_name}" || exit 1
+done
+
+exit 0
