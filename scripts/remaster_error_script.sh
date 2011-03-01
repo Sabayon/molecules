023e5b9ce20d9dbe1c83c2b3fb0aa6e191556cf7
Fabio Erculiani <lxnay@sabayon.org>
2011-03-01 11:04:26 +0100

    [scripts] improve reliability of remaster_error_script.sh

--- scripts/remaster_error_script.sh
+++ scripts/remaster_error_script.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
-if [ -d "${CHROOT_DIR}" ]; then
+if [ -d "${CHROOT_DIR}/proc" ]; then
+	echo "Unmounting ${CHROOT_DIR}/proc"
 	umount "${CHROOT_DIR}/proc"
 fi
 
-echo "Umounting bind to ${CHROOT_PKGS_DIR}"
-umount "${CHROOT_PKGS_DIR}" || exit 1
+CHROOT_PKGS_DIR="${CHROOT_DIR}/var/lib/entropy/client/packages"
+if [ -d "${CHROOT_PKGS_DIR}" ]; then
+	echo "Umounting bind to ${CHROOT_PKGS_DIR}"
+	umount "${CHROOT_PKGS_DIR}" || exit 1
+fi
