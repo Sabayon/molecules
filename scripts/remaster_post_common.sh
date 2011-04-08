dcd59a83f099df2d323aa0f229a1bdaaa6265ded
Fabio Erculiani <lxnay@sabayon.org>
2011-04-08 14:40:47 +0200

    [scripts] fix syntax error in remaster_post_common.sh

--- scripts/remaster_post_common.sh
+++ scripts/remaster_post_common.sh
@@ -2,7 +2,7 @@
 # BUILDING_DAILY is set in scripts/daily_iso_build.sh
 if [ -z "${BUILDING_DAILY}" ]; then
         # only the first occurence
-        local repo_conf="${CHROOT_DIR}/etc/entropy/repositories.conf"
+        repo_conf="${CHROOT_DIR}/etc/entropy/repositories.conf"
         sed -i "/^repository =/ s/sabayonlinux.org/sabayon-weekly/" "${repo_conf}" || exit 1
         sed -i "/^officialrepositoryid/ s/sabayonlinux.org/sabayon-weekly/" "${repo_conf}" || exit 1
         sed -i "/^official-repository-id/ s/sabayonlinux.org/sabayon-weekly/" "${repo_conf}" || exit 1
