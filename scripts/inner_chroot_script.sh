9583ba9b22c106e0ca41c46e63f6967458a5457a
Fabio Erculiani <lxnay@sabayon.org>
2011-09-05 07:42:26 +0200

    [scripts] create /proc and /dev/pts if they don't exist in source chroots

--- scripts/inner_chroot_script.sh
+++ scripts/inner_chroot_script.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# create /proc if it doesn't exist
+# rsync doesn't copy it
+if [ ! -d "/proc" ]; then
+	mkdir /proc
+	touch /proc/.keep
+fi
+if [ ! -d "/dev/pts" ]; then
+	mkdir /dev/pts
+	touch /dev/pts/.keep
+fi
+
 # copy /root defaults from /etc/skel
 rm -rf /root
 cp /etc/skel /root -Rap
