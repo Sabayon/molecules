b159a39e8979ab2c1fb70126ae1acb39edb40671
Fabio Erculiani <lxnay@sabayon.org>
2011-10-27 21:00:50 +0200

    [scripts] handle migration to sabayon-weekly for repositories.conf.d/ repos

--- scripts/remaster_post_common.sh
+++ scripts/remaster_post_common.sh
@@ -3,9 +3,17 @@
 if [ -z "${BUILDING_DAILY}" ]; then
         # only the first occurence
         repo_conf="${CHROOT_DIR}/etc/entropy/repositories.conf"
-        sed -i "/^repository =/ s/sabayonlinux.org/sabayon-weekly/" "${repo_conf}" || exit 1
         sed -i "/^officialrepositoryid/ s/sabayonlinux.org/sabayon-weekly/" "${repo_conf}" || exit 1
         sed -i "/^official-repository-id/ s/sabayonlinux.org/sabayon-weekly/" "${repo_conf}" || exit 1
+        sed -i "/^repository =/ s/sabayonlinux.org/sabayon-weekly/" "${repo_conf}" || exit 1
+	# new style repository config files (inside repositories.conf.d/ directory)
+	repo_conf_d="${CHROOT_DIR}/etc/entropy/repositories.conf.d/"
+	src_conf="${repo_conf_d}/entropy_sabayonlinux.org"
+	dst_conf="${repo_conf_d}/entropy_sabayon-weekly"
+	if [ -f "${src_conf}" ]; then
+		mv "${src_conf}" "${dst_conf}" || exit 1
+		sed -i "/^repository =/ s/= sabayonlinux.org/= sabayon-weekly/" "${dst_conf}" || exit 1
+	fi
 fi
 
 # remove entropy hwash
