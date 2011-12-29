a2f503bd4a82008c2c441c13b888a3f807d78b7f
Fabio Erculiani <lxnay@sabayon.org>
2011-12-29 23:00:27 +0100

    [scripts] post_mmc_image_script: chmod 644 the generated image tarball

--- scripts/post_mmc_image_script.sh
+++ scripts/post_mmc_image_script.sh
@@ -21,6 +21,8 @@ echo
 xz --compress --force "${IMAGE_PATH}" || exit 1
 [[ ! -f "${COMPRESSED_IMAGE_PATH}" ]] && { echo "${COMPRESSED_IMAGE_PATH} not found"; exit 1; }
 
+chmod 644 "${COMPRESSED_IMAGE_PATH}" || exit 1
+
 cd "$(dirname "${COMPRESSED_IMAGE_PATH}")" || exit 1
 img_name=$(basename "${COMPRESSED_IMAGE_PATH}")
 md5sum "${img_name}" > "${COMPRESSED_IMAGE_CHECKSUM_PATH}"
