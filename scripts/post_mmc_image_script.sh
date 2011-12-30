60b5c4218a0586e09cf2ef9b0e7d355f8a43d936
Fabio Erculiani <lxnay@sabayon.org>
2011-12-30 12:27:03 +0100

    [scripts] fixup image checksum generation

--- scripts/post_mmc_image_script.sh
+++ scripts/post_mmc_image_script.sh
@@ -9,13 +9,13 @@
 /usr/sbin/env-update && source /etc/profile
 
 COMPRESSED_IMAGE_PATH="${IMAGE_PATH}.xz"
-COMPRESSED_IMAGE_CHECKSUM_PATH="${IMAGE_CHECKSUM_PATH}.xz"
+COMPRESSED_IMAGE_CHECKSUM_PATH="${COMPRESSED_IMAGE_PATH}.md5"
 
 echo
 echo "Spawning xz --compress --force for:"
 echo "IMAGE_PATH = ${IMAGE_PATH}"
 echo "COMPRESSED_IMAGE_PATH = ${COMPRESSED_IMAGE_PATH}"
-echo "IMAGE_CHECKSUM_PATH = ${IMAGE_CHECKSUM_PATH}"
+echo "COMPRESSED_IMAGE_CHECKSUM_PATH = ${COMPRESSED_IMAGE_CHECKSUM_PATH}"
 echo
 
 xz --compress --force "${IMAGE_PATH}" || exit 1
@@ -25,6 +25,9 @@ chmod 644 "${COMPRESSED_IMAGE_PATH}" || exit 1
 
 cd "$(dirname "${COMPRESSED_IMAGE_PATH}")" || exit 1
 img_name=$(basename "${COMPRESSED_IMAGE_PATH}")
-md5sum "${img_name}" > "${COMPRESSED_IMAGE_CHECKSUM_PATH}"
+md5sum "${img_name}" > "${COMPRESSED_IMAGE_CHECKSUM_PATH}" || exit 1
+
+echo "All done"
+
 exit ${?}
 
