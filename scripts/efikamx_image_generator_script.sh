e7da2059922bdfcbe3edbd7c6d4b61324e7b6d69
Fabio Erculiani <lxnay@sabayon.org>
2012-02-14 18:03:04 +0100

    [scripts] disable tarball generation for Efika MX image

--- scripts/efikamx_image_generator_script.sh
+++ scripts/efikamx_image_generator_script.sh
@@ -4,5 +4,6 @@ export BOOT_PART_MKFS_ARGS="-L boot"
 export BOOT_PART_TYPE=ext3
 export BOOT_PART_TYPE_INSIDE_ROOT=1
 export ROOT_PART_TYPE=ext4
+export MAKE_TARBALL=0
 
 exec /sabayon/scripts/mkloopcard.sh /sabayon/scripts/mkloopcard_efikamx_chroot_hook.sh "$@"
