db20410c5c2f4a990d59431405e7e39467cdf783
Kelly Schwartz <wolfden@sabayon.org>
2012-05-04 08:39:56 -0500

    [forensics] fix typo in theme for setting background

--- remaster/gforensic/files/theme.lua
+++ remaster/gforensic/files/theme.lua
@@ -75,7 +75,7 @@ theme.titlebar_maximized_button_focus_active  = "/usr/share/awesome/themes/defau
 
 -- You can use your own command to set your wallpaper
 -- theme.wallpaper_cmd = { "awsetbg /usr/share/awesome/themes/default/background.png" }
-theme.wallpaper_cmd = { "awsetbg /usr/share/backgrounds/sabayonlinlux.png" }
+theme.wallpaper_cmd = { "awsetbg /usr/share/backgrounds/sabayonlinux.png" }
 
 -- You can use your own layout icons like this:
 theme.layout_fairh = "/usr/share/awesome/themes/default/layouts/fairhw.png"
