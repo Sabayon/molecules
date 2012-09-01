4fed801c40bcb91305000a6d51a507dd5a33f84e
Fabio Erculiani <lxnay@sabayon.org>
2012-09-01 13:48:26 +0200

    [scripts] signal stale processes to stderr

--- scripts/cleanup_pkgcache_locked.sh
+++ scripts/cleanup_pkgcache_locked.sh
@@ -3,10 +3,10 @@ SABAYON_MOLECULE_HOME="${SABAYON_MOLECULE_HOME:-/sabayon}"
 . "${SABAYON_MOLECULE_HOME}/scripts/iso_build.include"
 
 (
-    flock --timeout 7200 -x 9
+    flock --timeout ${LOCK_TIMEOUT} -x 9
     if [ "${?}" != "0" ]; then
         echo "[cleanup] cannot acquire lock, stale process holding it?" >&2
-        exit 1
+        kill_stale_process || exit 1
     fi
     "${SABAYON_MOLECULE_HOME}/scripts/cleanup_pkgcache.sh"
 ) 9> "${ISO_BUILD_LOCK}"
--- scripts/iso_build_locked.sh
+++ scripts/iso_build_locked.sh
@@ -12,20 +12,20 @@ SABAYON_MOLECULE_HOME="${SABAYON_MOLECULE_HOME:-/sabayon}"
 
 # Pull new data from Git
 (
-    flock --timeout 7200 -x 9
+    flock --timeout ${LOCK_TIMEOUT} -x 9
     if [ "${?}" != "0" ]; then
         echo "[git pull] cannot acquire lock, stale process holding it?" >&2
-        exit 1
+        kill_stale_process || exit 1
     fi
     cd /sabayon && git pull --quiet
 ) 9> "${ISO_BUILD_LOCK}"
 
 # Execute build
 (
-    flock --timeout 7200 -x 9
+    flock --timeout ${LOCK_TIMEOUT} -x 9
     if [ "${?}" != "0" ]; then
         echo "[build] cannot acquire lock, stale process holding it?" >&2
-        exit 1
+        kill_stale_process || exit 1
     fi
 
     "${SABAYON_MOLECULE_HOME}/scripts/${BUILD_SCRIPT_NAME}"
