32384949a662086512214d80c620b395eb1d4d88
Fabio Erculiani <lxnay@sabayon.org>
2012-10-10 00:16:29 +0200

    [scripts] improve fault-tolerance of "equo update"

--- scripts/remaster_generic_inner_chroot_script.sh
+++ scripts/remaster_generic_inner_chroot_script.sh
@@ -8,12 +8,16 @@ rm -f /var/run/entropy/entropy.lock
 
 export FORCE_EAPI=2
 updated=0
-for ((i=0; i < 6; i++)); do
+for ((i=0; i < 12; i++)); do
 	equo update && {
 		updated=1;
 		break;
 	}
-	sleep 1200 || exit 1
+	if [ ${i} -gt 6 ]; then
+		sleep 3600 || exit 1
+	else
+		sleep 1200 || exit 1
+	fi
 done
 if [ "${updated}" = "0" ]; then
 	exit 1
