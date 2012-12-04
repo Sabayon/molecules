54be8ed618ccd1a95c353a268e3f179543e99300
Fabio Erculiani <lxnay@sabayon.org>
2012-12-04 21:13:27 +0100

    [molecules*] move kernels on live images to /kernel (from /boot)
    This avoids path clashing with new grub2 UEFI SecureBoot stuff

--- scripts/generic_pre_iso_script.sh
+++ scripts/generic_pre_iso_script.sh
@@ -23,16 +23,16 @@ elif [ "${remaster_type}" = "ServerBase" ]; then
 	echo "ServerBase trigger, copying server kernel over"
 	boot_kernel=$(find "${CHROOT_DIR}/boot" -name "kernel-*" | sort | head -n 1)
 	boot_ramfs=$(find "${CHROOT_DIR}/boot" -name "initramfs-*" | sort | head -n 1)
-	cp "${boot_kernel}" "${CDROOT_DIR}/boot/sabayon" || exit 1
-	cp "${boot_ramfs}" "${CDROOT_DIR}/boot/sabayon.igz" || exit 1
+	cp "${boot_kernel}" "${CDROOT_DIR}/kernel/sabayon" || exit 1
+	cp "${boot_ramfs}" "${CDROOT_DIR}/kernel/sabayon.igz" || exit 1
 	isolinux_source="${SABAYON_MOLECULE_HOME}/remaster/serverbase_isolinux.cfg"
 	grub_source="${SABAYON_MOLECULE_HOME}/remaster/serverbase_grub.cfg"
 elif [ "${remaster_type}" = "HardenedServer" ]; then
 	echo "HardenedServer trigger, copying server kernel over"
 	boot_kernel=$(find "${CHROOT_DIR}/boot" -name "kernel-*" | sort | head -n 1)
 	boot_ramfs=$(find "${CHROOT_DIR}/boot" -name "initramfs-*" | sort | head -n 1)
-	cp "${boot_kernel}" "${CDROOT_DIR}/boot/sabayon" || exit 1
-	cp "${boot_ramfs}" "${CDROOT_DIR}/boot/sabayon.igz" || exit 1
+	cp "${boot_kernel}" "${CDROOT_DIR}/kernel/sabayon" || exit 1
+	cp "${boot_ramfs}" "${CDROOT_DIR}/kernel/sabayon.igz" || exit 1
 	isolinux_source="${SABAYON_MOLECULE_HOME}/remaster/hardenedserver_isolinux.cfg"
 	grub_source="${SABAYON_MOLECULE_HOME}/remaster/hardenedserver_grub.cfg"
 fi
