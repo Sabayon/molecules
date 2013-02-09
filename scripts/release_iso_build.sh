276cf0e763ea2758950b57297b5c85f7c1f6047b
Fabio Erculiani <lxnay@sabayon.org>
2013-02-09 23:24:26 +0100

    [scripts] add release_iso_build*.sh script to easily make Sabayon releases

---
+++ scripts/release_iso_build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Path to molecules.git dir
+SABAYON_MOLECULE_HOME="${SABAYON_MOLECULE_HOME:-/sabayon}"
+export SABAYON_MOLECULE_HOME
+
+SABAYON_RELEASE="${1}"
+if [ -z "${SABAYON_RELEASE}" ]; then
+	echo "${0} <release version>" >&2
+	exit 1
+fi
+shift
+
+export SABAYON_RELEASE
+exec "${SABAYON_MOLECULE_HOME}"/scripts/iso_build.sh "release" "$@"
