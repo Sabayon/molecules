73260a60affdbf20116de77888245a2ddfbb8b80
Fabio Erculiani <lxnay@sabayon.org>
2013-02-09 23:29:21 +0100

    [scripts] make torrents for both monthly and release targets

--- scripts/monthly_iso_build.sh
+++ scripts/monthly_iso_build.sh
@@ -3,5 +3,6 @@
 # Path to molecules.git dir
 SABAYON_MOLECULE_HOME="${SABAYON_MOLECULE_HOME:-/sabayon}"
 export SABAYON_MOLECULE_HOME
+export MAKE_TORRENTS="1"
 
 exec "${SABAYON_MOLECULE_HOME}"/scripts/iso_build.sh "monthly" "$@"
--- scripts/release_iso_build.sh
+++ scripts/release_iso_build.sh
@@ -3,6 +3,7 @@
 # Path to molecules.git dir
 SABAYON_MOLECULE_HOME="${SABAYON_MOLECULE_HOME:-/sabayon}"
 export SABAYON_MOLECULE_HOME
+export MAKE_TORRENTS="1"
 
 SABAYON_RELEASE="${1}"
 if [ -z "${SABAYON_RELEASE}" ]; then
