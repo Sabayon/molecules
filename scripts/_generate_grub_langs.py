67eafe23e8c223773fbc1a13a49accf8fc43ee7e
Fabio Erculiani <lxnay@sabayon.org>
2013-03-06 17:20:19 +0000

    [scripts] fix keyboard mapping grub.cfg menus generation, related to bug 3628

--- scripts/_generate_grub_langs.py
+++ scripts/_generate_grub_langs.py
@@ -72,11 +72,113 @@ langs = [
     [ "zh_TW", "zh_TW", "中文(繁體)" ],
 ]
 
+keymaps = [
+    [ "af", "Afghanistan"],
+    [ "al", "Albania"],
+    [ "ad", "Andorra"],
+    [ "ara", "Arabic"],
+    [ "am", "Armenia"],
+    [ "es_ast", "Asturian"],
+    [ "az", "Azerbaijan"],
+    [ "bd", "Bangladesh"],
+    [ "by", "Belarus"],
+    [ "be", "Belgium"],
+    [ "bt", "Bhutan"],
+    [ "ba", "Bosnia"],
+    [ "br", "Brazil"],
+    [ "bg", "Bulgaria"],
+    [ "kh", "Cambodia"],
+    [ "ca", "Canada"],
+    [ "es_cat", "Catalan"],
+    [ "cn", "China"],
+    [ "cd", "Congo"],
+    [ "hr", "Croatia"],
+    [ "cz", "Czechia"],
+    [ "dk", "Denmark"],
+    [ "us_dvorak", "Dvorak"],
+    [ "epo", "Esperanto"],
+    [ "ee", "Estonia"],
+    [ "et", "Ethiopia"],
+    [ "fo", "Faroes"],
+    [ "fi", "Finland"],
+    [ "fr_oss", "France"],
+    [ "ge", "Georgia"],
+    [ "de", "Germany"],
+    [ "gh", "Ghana"],
+    [ "gr", "Greece"],
+    [ "gn", "Guinea"],
+    [ "in_guj", "Gujarati"],
+    [ "in_guru", "Gurmukhi"],
+    [ "hu", "Hungary"],
+    [ "is", "Iceland"],
+    [ "in", "India"],
+    [ "ir", "Iran"],
+    [ "iq", "Iraq"],
+    [ "ie", "Ireland"],
+    [ "il", "Israel"],
+    [ "it", "Italy"],
+    [ "jp", "Japan"],
+    [ "in_kan", "Kannada"],
+    [ "kz", "Kazakhstan"],
+    [ "kr", "Korea"],
+    [ "tr_ku", "Kurdish"],
+    [ "kg", "Kyrgyzstan"],
+    [ "la", "Laos"],
+    [ "latam", "Latin Amer."],
+    [ "lv", "Latvia"],
+    [ "lt", "Lithuania"],
+    [ "mk", "Macedonia"],
+    [ "in_mal", "Malayalam"],
+    [ "mv", "Maldives"],
+    [ "mt", "Malta"],
+    [ "mao", "Maori"],
+    [ "mn", "Mongolia"],
+    [ "me", "Montenegro"],
+    [ "ma", "Morocco"],
+    [ "mm", "Myanmar"],
+    [ "np", "Nepal"],
+    [ "nl", "Netherlands"],
+    [ "ng", "Nigeria"],
+    [ "no", "Norway"],
+    [ "pk", "Pakistan"],
+    [ "pl", "Poland"],
+    [ "pt", "Portugal"],
+    [ "ro", "Romania"],
+    [ "ru", "Russia"],
+    [ "fi_smi", "Saami (Fin.)"],
+    [ "no_smi", "Saami (Nor.)"],
+    [ "se_smi", "Saami (Swe.)"],
+    [ "sn", "Senegal"],
+    [ "rs", "Serbia"],
+    [ "sk", "Slovakia"],
+    [ "si", "Slovenia"],
+    [ "za", "South Africa"],
+    [ "es", "Spain"],
+    [ "lk", "Sri Lanka"],
+    [ "se", "Sweden"],
+    [ "ch_fr", "Swiss French"],
+    [ "ch", "Swiss German"],
+    [ "sy", "Syria"],
+    [ "tj", "Tajikistan"],
+    [ "in_tam", "Tamil"],
+    [ "in_tel", "Telugu"],
+    [ "th", "Thailand"],
+    [ "tr", "Turkey"],
+    [ "tr_f", "Turkey (F)"],
+    [ "tm", "Turkmenistan"],
+    [ "gb", "UK"],
+    [ "us", "USA"],
+    [ "us_intl", "USA Intl."],
+    [ "ua", "Ukraine"],
+    [ "uz", "Uzbekistan"],
+    [ "vn", "Vietnam"],
+]
+
 print("""\
 submenu "Language Selection" {
 """)
 
-for keymap, lang, name in langs:
+for shortlang, lang, name in langs:
     print("""\
    menuentry "%(name)s" {
       echo "Switching to: $chosen"
@@ -96,7 +198,7 @@ print("""\
 submenu "Keyboard Selection" {
 """)
 
-for keymap, lang, name in langs:
+for keymap, name in keymaps:
     print("""\
    menuentry "%(name)s" {
       echo "Switching to: $chosen"
