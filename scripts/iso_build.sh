aeb70e3f87adb6c1c4483b3c34ae2a3c59810c93
Fabio Erculiani <lxnay@sabayon.org>
2013-04-28 19:02:25 +0200

    [scripts] iso_build.sh: limit upload speed to 2Mb/sec

--- scripts/iso_build.sh
+++ scripts/iso_build.sh
@@ -317,7 +317,7 @@ move_to_mirrors() {
 		sleepnight
 		rm -f "${do_push}"
 
-		safe_run 5 rsync -av --partial --delete-excluded \
+		safe_run 5 rsync -av --partial --bwlimit=2048 --delete-excluded \
 			"${SABAYON_MOLECULE_HOME}"/iso_rsync/*"${ISO_TAG}"* \
 			"${ssh_path}/rsync.sabayon.org/iso/${ISO_DIR}" \
 			|| return 1
