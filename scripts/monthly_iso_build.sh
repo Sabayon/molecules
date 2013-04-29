83ed1aef5991c5fb2ea040eff95a8f7bca10b395
Fabio Erculiani <lxnay@sabayon.org>
2013-04-29 12:00:10 +0100

    [scripts] monthly_iso_build.sh: do not export MAKE_TORRENTS

--- scripts/monthly_iso_build.sh
+++ scripts/monthly_iso_build.sh
@@ -3,6 +3,5 @@
 # Path to molecules.git dir
 SABAYON_MOLECULE_HOME="${SABAYON_MOLECULE_HOME:-/sabayon}"
 export SABAYON_MOLECULE_HOME
-export MAKE_TORRENTS="1"
 
 exec "${SABAYON_MOLECULE_HOME}"/scripts/iso_build.sh "monthly" "$@"
