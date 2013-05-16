0a81c4727aab7a791b5d958cdcfae2b3942dd1be
Fabio Erculiani <lxnay@sabayon.org>
2013-05-16 11:14:02 +0200

    [scripts] remaster_pre: mkdir /var/lib/entropy/client/packages before mount --bind

--- scripts/remaster_pre.sh
+++ scripts/remaster_pre.sh
@@ -11,8 +11,8 @@ CHROOT_PKGS_DIR="${CHROOT_DIR}/var/lib/entropy/client/packages"
 [[ ! -d "${CHROOT_PKGS_DIR}" ]] && mkdir -p "${CHROOT_PKGS_DIR}"
 
 # make sure it's all clean before mounting
-rm -rf "${CHROOT_PKGS_DIR}"/*
 echo "Mounting bind to ${CHROOT_PKGS_DIR}"
+mkdir -p "${CHROOT_PKGS_DIR}" || exit 1
 mount --bind "${PKGS_DIR}" "${CHROOT_PKGS_DIR}" || exit 1
 
 content=$(ls -1 "${CHROOT_DIR}/proc" | wc -l)
