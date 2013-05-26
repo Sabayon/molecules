81e6d751db7be856912c6bda14127d2206191c7f
Fabio Erculiani <lxnay@sabayon.org>
2013-05-26 07:57:27 +0200

    [boot] beaglebone: migrate to devicetree, update u-boot

--- scripts/beaglebone_image_generator_script.sh
+++ scripts/beaglebone_image_generator_script.sh
@@ -4,4 +4,6 @@
 SABAYON_MOLECULE_HOME="${SABAYON_MOLECULE_HOME:-/sabayon}"
 export SABAYON_MOLECULE_HOME
 
+export DTB_FILES="am335x-bone.dtb"
+
 exec "${SABAYON_MOLECULE_HOME}"/scripts/mkloopcard.sh "${SABAYON_MOLECULE_HOME}"/scripts/mkloopcard_beaglebone_chroot_hook.sh "$@"
