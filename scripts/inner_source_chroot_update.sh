6773399ef673d925385c812bc24a842fdd5dcfec
Fabio Erculiani <lxnay@sabayon.org>
2013-10-25 19:16:07 +0200

    [scripts] inner_source_chroot_update, remove /run/.eselect-init.run
    This fixes bug #4457

--- scripts/inner_source_chroot_update.sh
+++ scripts/inner_source_chroot_update.sh
@@ -87,6 +87,7 @@ rm -rf /var/lib/entropy/client/packages
 
 # Make sure that systemd is still the default init system
 eselect init set systemd
+rm -f /run/.eselect-init.run
 
 # copy Portage config from sabayonlinux.org entropy repo to system
 for conf in package.mask package.unmask package.keywords make.conf package.use; do
