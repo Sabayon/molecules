1a1e25db1e12d0cfb6062442c26b2c054550bcbf
Fabio Erculiani <lxnay@sabayon.org>
2014-11-30 15:42:53 +0000

    [scripts] create safe_run() and use it

--- scripts/remaster_generic_inner_chroot_script.sh
+++ scripts/remaster_generic_inner_chroot_script.sh
@@ -3,6 +3,25 @@
 /usr/sbin/env-update
 . /etc/profile
 
+safe_run() {
+	local updated=0
+	for ((i=0; i < 42; i++)); do
+		"${@}" && {
+			updated=1;
+			break;
+		}
+		if [ ${i} -gt 6 ]; then
+			sleep 3600 || return 1
+		else
+			sleep 1200 || return 1
+		fi
+	done
+	if [ "${updated}" = "0" ]; then
+		return 1
+	fi
+	return 0
+}
+
 # make sure there is no stale pid file around that prevents entropy from running
 rm -f /var/run/entropy/entropy.lock
 
@@ -17,19 +36,4 @@ for repo_conf in /etc/entropy/repositories.conf.d/entropy_*; do
 		-e "/garr.it/p" -e "/^\[.*\]$/p" -i "${repo_conf}"
 done
 
-export FORCE_EAPI=2
-updated=0
-for ((i=0; i < 42; i++)); do
-	equo update && {
-		updated=1;
-		break;
-	}
-	if [ ${i} -gt 6 ]; then
-		sleep 3600 || exit 1
-	else
-		sleep 1200 || exit 1
-	fi
-done
-if [ "${updated}" = "0" ]; then
-	exit 1
-fi
+FORCE_EAPI=2 safe_run equo update || exit 1
