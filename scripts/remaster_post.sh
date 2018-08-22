9273a02fb2c7afc35be52f56ba49a9d7f65bab7c
Geaaru <geaaru@sabayonlinux.org>
2018-08-22 08:26:45 +0200

    Fix use of multiple outer_chroot_script_after.
    Workaround related with an issue on molecules that instead
    of call both script defined as outer_chroot_script_after
    try to call first and second it calls first with second
    as argument.

--- scripts/remaster_post.sh
+++ scripts/remaster_post.sh
@@ -7,6 +7,19 @@ export SABAYON_MOLECULE_HOME
 PKGS_DIR="${SABAYON_MOLECULE_HOME}/pkgcache"
 CHROOT_PKGS_DIR="${CHROOT_DIR}/var/lib/entropy/client/packages"
 
+echo "================================================="
+echo "REMASTER_POST.SH ARGS: $@"
+echo "================================================="
+if [ $# -gt 0 ] ; then
+  if [ -f "$1" ] ; then
+    echo "================================================="
+    echo "REMASTER_POST.SH ARGS: Executing $1..."
+    echo "================================================="
+    . $1
+    echo "================================================="
+  fi
+fi
+
 # load common stuff
 . "${SABAYON_MOLECULE_HOME}"/scripts/remaster_post_common.sh
 
--- scripts/xfce_remaster_post.sh
+++ scripts/xfce_remaster_post.sh
@@ -4,7 +4,16 @@
 SABAYON_MOLECULE_HOME="${SABAYON_MOLECULE_HOME:-/sabayon}"
 export SABAYON_MOLECULE_HOME
 
+echo "================================================"
+echo "ENTER ON XFCE-REMASTER-POST.SH"
+echo "================================================"
+
 GFORENSIC_DIR="${SABAYON_MOLECULE_HOME}/remaster/gforensic"
+
+echo "================================================"
+ls -l ${GFORENSIC_DIR}
+echo "================================================"
+
 # setup skel and background
 cp -v "${GFORENSIC_DIR}"/usr/share/backgrounds/sabayon-forensic.png "${CHROOT_DIR}/usr/share/backgrounds/"
 cp -v "${GFORENSIC_DIR}"/usr/share/backgrounds/sabayon-forensic.jpg "${CHROOT_DIR}/usr/share/backgrounds/"
@@ -16,3 +25,7 @@ cp -rv "${GFORENSIC_DIR}"/files/Qogir-dark "${CHROOT_DIR}/usr/share/themes/"
 cp -v "${GFORENSIC_DIR}"/files/forensic-desktops/* "${CHROOT_DIR}/usr/share/applications/"
 cp -v "${GFORENSIC_DIR}"/files/org.sabayon.gschema.override "${CHROOT_DIR}/usr/share/glib-2.0/schemas/org.sabayon.gschema.override"
 cp -v "${GFORENSIC_DIR}"/files/org.gnome.shell.extensions.dash-to-dock.gschema.xml "${CHROOT_DIR}/usr/share/glib-2.0/schemas/org.gnome.shell.extensions.dash-to-dock.gschema.xml"
+
+echo "================================================"
+echo "EXIT FROM XFCE-REMASTER-POST.SH"
+echo "================================================"
