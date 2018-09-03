3594a268cc90d12c76f6821b994d55fd67eae06d
wolfden <kjs@photosbykjs.us>
2018-09-03 11:34:02 -0500

    remove gsettings and added efi forensics graphic for grub

--- scripts/xfce_remaster_post.sh
+++ scripts/xfce_remaster_post.sh
@@ -18,11 +18,6 @@ echo "================================================"
 cp -rv "${GFORENSIC_DIR}"/usr "${CHROOT_DIR}/"
 cp -rv "${GFORENSIC_DIR}"/etc "${CHROOT_DIR}/"
 glib-compile-schemas "${CHROOT_DIR}/usr/share/glib-2.0/schemas/"
-gsettings set org.gnome.shell.extensions.dash-to-dock extended-height true
-gsettings set org.gnome.shell.extensions.dash-to-dock dock-position LEFT
-gsettings set org.gnome.shell.extensions.dash-to-dock transparency-mode DYNAMIC
-gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 32
-gsettings set org.gnome.shell.extensions.dash-to-dock custom-theme-shrink true
 
 echo "================================================"
 echo "EXIT FROM XFCE-REMASTER-POST.SH"
